c447641a24cdf8a154bf721c66df87151005593a
Kevin Gonzalez <typekev@gmail.com>
2019-12-23 15:36:29 +0100

    Add genposts script

---
+++ genposts.py
@@ -0,0 +1,20 @@
+import os
+import json
+import frontmatter
+
+dict = {}
+
+for file in os.listdir("./posts"):
+    if file.endswith(".md"):
+        post = frontmatter.load('./posts/' + file)
+        metadata = post.metadata
+        date_published = metadata["published-on"].strftime("%b %d, %Y")
+        obj = {**metadata, "published-on": date_published}
+        content = post.content
+        with open('./posts/json/' + file + ".json", 'w') as file_descriptor:
+                json.dump({**obj, "content": content}, file_descriptor)
+
+        dict = {**dict, file: {**metadata, "published-on": date_published}}
+
+with open('./posts/json/posts.json', 'w') as file_descriptor:
+         json.dump(dict, file_descriptor)

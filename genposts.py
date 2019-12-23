3ca49836f66a3f2c1c9ba7bb74c889b4c41a54ba
Kevin Gonzalez <typekev@gmail.com>
2019-12-23 16:22:15 +0100

    genposts script should return time stamp

--- genposts.py
+++ genposts.py
@@ -1,21 +1,23 @@
 import os
 import json
 import frontmatter
+from datetime import datetime
 
 dict = {}
 
 for file in os.listdir("./posts"):
     if file.endswith(".md"):
-        post = frontmatter.load('./posts/' + file)
+        post = frontmatter.load("./posts/" + file)
         metadata = post.metadata
-        date_published = metadata["published-on"].strftime("%b %d, %Y")
-        obj = {**metadata, "published-on": date_published}
+        date_published = metadata["published"].strftime("%b %d, %Y")
+        timestamp = datetime.timestamp(metadata["published"])
+        obj = {**metadata, "published": date_published, "timestamp": int(timestamp)}
         content = post.content
-        
-        with open('./posts/json/' + file + ".json", 'w') as file_descriptor:
-                json.dump({**obj, "content": content}, file_descriptor)
 
-        dict = {**dict, file: {**metadata, "published-on": date_published}}
+        with open("./posts/json/" + file + ".json", "w") as file_descriptor:
+            json.dump({**obj, "content": content}, file_descriptor)
 
-with open('./posts/json/index.json', 'w') as file_descriptor:
-         json.dump(dict, file_descriptor)
+        dict = {**dict, file: obj}
+
+with open("./posts/json/index.json", "w") as file_descriptor:
+    json.dump(dict, file_descriptor)

4fc3091b931584ba109e5da3c2d283f88b05a0fd
Kevin Gonzalez <typekev@gmail.com>
2022-01-01 18:06:28 +0100

    Adds post id and md location to json objects

--- genposts.py
+++ genposts.py
@@ -5,30 +5,40 @@ from datetime import datetime
 
 POST_DIR = "./posts/"
 POST_JSON_DIR = POST_DIR + "json/"
+LOCATION_MD_BASE_URL = "https://raw.githubusercontent.com/typekev/typekev-blog/master/posts"
+LOCATION_JSON_BASE_URL = f"{LOCATION_MD_BASE_URL}/json"
 
-dict = {}
+posts = {}
+
+def get_md_location(id):
+    return f"{LOCATION_MD_BASE_URL}/{id}.md"
+
+def get_json_location(id):
+    return f"{LOCATION_JSON_BASE_URL}/{id}.json"
 
 if not os.path.exists(POST_JSON_DIR):
     os.makedirs(POST_JSON_DIR)
 
 for file in os.listdir(POST_DIR):
     if file.endswith(".md"):
+        id = file[0:-3]
         post = frontmatter.load(POST_DIR + file)
         metadata = post.metadata
         date_published = metadata["published"].strftime("%b %d, %Y")
         timestamp = datetime.timestamp(metadata["published"])
-        obj = {
+        content = post.content
+        post_data = {
             **metadata,
+            "id": id,
             "published": date_published,
             "timestamp": int(timestamp),
-            "location": file + ".json",
+            "location": {"md": get_md_location(id), "json": get_json_location(id)}
         }
-        content = post.content
 
-        with open(POST_JSON_DIR + file + ".json", "w") as file_descriptor:
-            json.dump({**obj, "content": content}, file_descriptor)
+        with open(POST_JSON_DIR + id + ".json", "w") as file_descriptor:
+            json.dump({**post_data, "content": content}, file_descriptor)
 
-        dict = {**dict, file: obj}
+        posts = {**posts, id: post_data}
 
 with open(POST_JSON_DIR + "index.json", "w") as file_descriptor:
-    json.dump(dict, file_descriptor)
+    json.dump(posts, file_descriptor)
